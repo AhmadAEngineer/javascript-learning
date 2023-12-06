04d93b337e9d702a2757d7605b31695c235cbd2e
ahmad <ahmadaengineer@gmail.com>
2023-12-06 10:08:18 +0500

    Chapter two completed

---
+++ chapter_three/data_types.js
@@ -0,0 +1,12 @@
+// There are two categories of data types in javaScript
+// 1. primitive types 2. object types
+// Primitive types contains numbers, strings, booleans, and special values 'null' and 'undefined'
+// ES6 adds a new special purpose type, known as Symbol, that enables the definition of language
+// extensions without harming backward compatibility.
+
+let name = "Ahmad";
+name = "great";
+console.log(name);
+let father;
+name = father;
+console.log(name);
